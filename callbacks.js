6d73a18bdb90e62ef9055b943cbd7ea1ae5dee33
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-06-29 09:30:08 -0700

    closure

--- callbacks.js
+++ callbacks.js
@@ -1,8 +1,18 @@
-window.setTimeout(function(){
-	callback();
-}, 5000);
+// window.setTimeout(function(){
+// 	callback();
+// }, 5000);
 
-callback = function() {
-	alert('HAMMERTIME');
+hammerTime = function(time){
+	window.setTimeout(function(){
+	alert(`${time} is hammertime!`);
+}, 500);
 };
+
+// callback = function(time) {
+// 	alert(`${time} is hammertime!`);
+// };
+
+hammerTime("Dec 12th");
+
+
 
