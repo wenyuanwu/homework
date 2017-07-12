494cbc0ba32991f58769302b1ff8e8d72685c704
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-07-11 23:57:03 -0700

    middleware

--- Middleware/frontend/todo_redux.jsx
+++ Middleware/frontend/todo_redux.jsx
@@ -8,7 +8,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  
+  store = applyMiddlewares(store, addLoggingToDispatch);
+
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = store => next => action => {
+	console.log(store.getState());
+	console.log(action);
+	next(action);
+	console.log(store.getState());
+}
+
+const applyMiddlewares = (store, ...middlewares) => {
+   let dispatch = store.dispatch;
+   middlewares.forEach((middleware) => {
+     dispatch = middleware(store)(dispatch);
+   });
+   return Object.assign({}, store, { dispatch });
+ };
+
+
