82f4130f3a072be4fb0390193fc25e8688fb654c
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-07-12 18:55:17 -0700

    rainbow router

---
+++ Giphy Search/giphy_search.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+import {fetchSearchGiphys} from './util/api_util';
+import {receiveSearchGiphys} from './actions/giphy_actions';
+
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	const root = document.getElementById('root');
+	const store = configureStore();
+	window.store = store;
+	window.fetchSearchGiphys = fetchSearchGiphys;
+	window.receiveSearchGiphys = receiveSearchGiphys;
+	ReactDOM.render(<Root store={store}/>, root);
+});
+
+
+
---
+++ Rainbow Routes/components/red.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        <Link to='/red' >Red</Link>
+
+        <Route path="/red/orange" component={Orange} />
+      </div>
+    );
+  }
+};
+
+export default Red;
